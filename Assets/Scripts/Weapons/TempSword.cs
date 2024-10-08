83641454bd219c13e066d95704a4fbcc0c37aaff
ZubairAY <zubair.ahmed@se22.qmul.ac.uk>
2024-10-08 19:53:16 +0100

    Added Enemy Tracking

---
+++ Assets/Scripts/Weapons/TempSword.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempSword : MeleeWeapon
+
+{
+    // weaponName = "Swing-a-ling"
+    // attackDistance = 3f;
+    // attacking = false;
+    // attackSpeed = 1f;
+    // attackDamage = 15f;
+}

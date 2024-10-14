b0e36c0b0f49ed0ceba30ca6705e3fc6935d6c64
David Ghorbani-Nia <d.ghorbani-nia@se22.qmul.ac.uk>
2024-10-14 15:10:56 +0100

    updates

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    private float health = 100f;
+    private Collider _collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+
+    public float GetHealth()
+    {
+        return health;
+    }
+    public void SetHealth(float value)
+    {
+        health = value;
+        if(health <= 0)
+        {
+            Death();
+        }   
+    }
+    private void Death()
+    {
+        Debug.Log("Enemy Died");
+        
+    }
+}
--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Weaponmanager : MonoBehaviour
+
 {
     private static readonly int Blocking = Animator.StringToHash("Blocking");
     private static readonly int Attacking = Animator.StringToHash("Attacking");
@@ -12,6 +13,9 @@ public class Weaponmanager : MonoBehaviour
 
     [SerializeField] private GameObject weaponPrefab;
 
+    [SerializeField] private GameObject weaponColliderParent;
+    private Collider weaponCollider;
+
     private Shield currentShield;
     private bool attacking = false;
     private bool blocking = false;
@@ -19,6 +23,7 @@ public class Weaponmanager : MonoBehaviour
     private void Awake()
     {
         anim = weaponPrefab.GetComponent<Animator>();
+        weaponCollider = weaponColliderParent.GetComponent<Collider>();
     }
 
     public void Attack()
@@ -47,5 +52,6 @@ public class Weaponmanager : MonoBehaviour
     {
 
     }
+
 
 }

b72ee95305cf35efdefb35eb4a04f830468bdded
Sepehr <main.sepehr@gmail.com>
2024-10-18 12:17:58 +0100

    Inventory System - Added Equipment Management

--- Assets/Scripts/InventoryScripts/Inventory.cs
+++ Assets/Scripts/InventoryScripts/Inventory.cs
@@ -59,42 +59,60 @@ namespace InventoryScripts
         }
         
         // Equipment Setters
-        public void Equip(Equipment equipment)
+        public EquipmentType Equip(Equipment equipment)
         {
             if (equipment.type == EquipmentType.ARMOR)
             {
                 _equippedArmour = equipment;
                 _equippedArmour.Equip();
+                return EquipmentType.ARMOR;
             }
-            else if (equipment.type == EquipmentType.OFFHAND)
+
+            if (equipment.type == EquipmentType.OFFHAND)
             {
                 _equippedOffHand = equipment;
                 _equippedOffHand.Equip();
+                return EquipmentType.OFFHAND;
             }
-            else if (equipment.type == EquipmentType.MAINHAND)
+
+            if (equipment.type == EquipmentType.MAINHAND)
             {
                 _equippedMainHand = equipment;
                 _equippedMainHand.Equip();
+                return EquipmentType.MAINHAND;
             }
+
+            return EquipmentType.NONE;
         }
 
-        public void Unequip(Equipment equipment)
+        public EquipmentType Unequip(Equipment equipment)
         {
             if (equipment == _equippedArmour)
             {
                 _equippedArmour.Unequip();
                 _equippedArmour = null;
+                return EquipmentType.ARMOR;
             }
-            else if (equipment == _equippedOffHand)
+
+            if (equipment == _equippedOffHand)
             {
                 _equippedOffHand.Unequip();
                 _equippedOffHand = null;
+                return EquipmentType.OFFHAND;
             }
-            else if (equipment == _equippedMainHand)
+            if (equipment == _equippedMainHand)
             {
                 _equippedMainHand.Unequip();
                 _equippedMainHand = null;
+                return EquipmentType.MAINHAND;
             }
+
+            return EquipmentType.NONE;
+        }
+
+        public InventoryItem GetItemFromIndex(int i)
+        {
+            return _inventoryArray[i].Peek();
         }
     }
 }
--- Assets/Scripts/InventoryScripts/InventoryManager.cs
+++ Assets/Scripts/InventoryScripts/InventoryManager.cs
@@ -1,13 +1,15 @@
+using GameplayMechanics.Effects;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace InventoryScripts
 {
     public class InventoryManager : MonoBehaviour
     {
-        [SerializeField] private GameObject[] _inventoryItemsUI = new GameObject[3];
-        [SerializeField] private GameObject[] _equippedItemsUI = new GameObject[3];
+        [FormerlySerializedAs("_inventoryItemsUI")] public GameObject[] inventoryItemsUI = new GameObject[3];
+        [FormerlySerializedAs("_equippedItemsUI")] public GameObject[] equippedItemsUI = new GameObject[3];
         // Start is called before the first frame update
         void Start()
         {
@@ -22,13 +24,43 @@ namespace InventoryScripts
 
         private void AddToInventoryUI(InventoryItem inventoryItem, int i)
         {
-            if (i < 0 || i >= _inventoryItemsUI.Length)
+            if (i < 0 || i >= inventoryItemsUI.Length)
             {
                 Debug.Log("Inventory out of space!");
                 return;
             }
-            _inventoryItemsUI[i].GetComponent<TextMeshProUGUI>().
+            inventoryItemsUI[i].GetComponent<TextMeshProUGUI>().
                 SetText(inventoryItem.gameItem.GetName()[0].ToString());
+        }
+
+        public void MoveToEquipment(InventoryItem inventoryItem, int i)
+        {
+            inventoryItemsUI[i].GetComponent<TextMeshProUGUI>().SetText("");
+            InventoryItem item = Inventory.Instance.Pop(inventoryItem);
+            EquipmentType type = Inventory.Instance.Equip(item.equipment);
+            Debug.Log(type);
+            Debug.Log(item.gameItem.GetName());
+
+            if (type == EquipmentType.NONE)
+            {
+                Debug.Log("Item is not equippable.");
+            }
+            else if (type == EquipmentType.MAINHAND)
+            {
+                equippedItemsUI[0].GetComponent<TextMeshProUGUI>()
+                    .SetText(item.gameItem.GetName()[0].ToString());
+            }
+            else if (type == EquipmentType.ARMOR)
+            {
+                equippedItemsUI[1].GetComponent<TextMeshProUGUI>()
+                    .SetText(item.gameItem.GetName()[0].ToString());
+            }
+            else if (type == EquipmentType.OFFHAND)
+            {
+                equippedItemsUI[2].GetComponent<TextMeshProUGUI>()
+                    .SetText(item.gameItem.GetName()[0].ToString());
+            }
+            
         }
     }
 }

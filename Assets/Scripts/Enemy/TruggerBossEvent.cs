aff21fd6ff95758d435ffb41cca4549751a4238f
ZubairAY <zubair.ahmed@se22.qmul.ac.uk>
2024-12-30 19:24:38 +0000

    Bug Fix

--- Assets/Scripts/Enemy/TruggerBossEvent.cs
+++ Assets/Scripts/Enemy/TruggerBossEvent.cs
@@ -8,19 +8,20 @@ public class TruggerBossEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other != null && other.gameObject.CompareTag("Player"))
         {
             GameObject.FindWithTag("Boss").GetComponent<AbstractEnemy>().SetState(EnemyState.TRIGGERED);
+            Destroy(gameObject);
         }
     }
 }

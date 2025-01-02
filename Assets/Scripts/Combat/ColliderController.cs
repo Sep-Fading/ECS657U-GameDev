c91b6bcc846f9b435801b2b808e2e26483dcaedf
Scattz1 <davidukno1@hotmail.com>
2025-01-02 23:32:16 +0000

    added anim events

---
+++ Assets/Scripts/Combat/ColliderController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using InventoryScripts;
+namespace Combat
+{
+    public class ColliderController : MonoBehaviour
+    {
+        public void EnableCollider()
+        {
+            if (Inventory.Instance.EquippedMainHand.GetGameObject().GetComponent<Collider>().enabled != true)
+            {
+                Inventory.Instance.EquippedMainHand.GetGameObject().GetComponent<Collider>().enabled = true;
+            }
+        }
+        public void DisableCollider()
+        {
+            if (Inventory.Instance.EquippedMainHand.GetGameObject().GetComponent<Collider>().enabled)
+            {
+                Inventory.Instance.EquippedMainHand.GetGameObject().GetComponent<Collider>().enabled = false;
+            }
+        }
+    }
+}
